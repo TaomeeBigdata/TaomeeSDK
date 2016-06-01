0460302edde4a4039f8802f76c4aadde670a6076
hj022000 <2500833119@qq.com>
2016-06-01 17:44:53 +0800

    权限赋值

--- src/com/taomee/statlogger/StatLogger.java
+++ src/com/taomee/statlogger/StatLogger.java
@@ -927,6 +927,7 @@ public class StatLogger {
 			if (!file.canExecute()) {
 				file.setExecutable(true);
 				// System.out.println("设置文件的可执行权限");
+				
 				Runtime.getRuntime().exec("chmod 777 -R "+file);
 			}
 
